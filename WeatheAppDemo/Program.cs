42b8ce367ac33e71049d9f83b8c9fe35e5140f96
dev.alyzahran95 <dev.alyzahran95@gmail.com>
2024-08-17 14:57:55 +0300

    simple change

--- WeatheAppDemo/Program.cs
+++ WeatheAppDemo/Program.cs
@@ -4,8 +4,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapControllers();
 app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 
 //app.MapGet("/", () => "Hello World!");
 
