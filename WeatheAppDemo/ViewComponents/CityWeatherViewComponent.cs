a39be37ddc9c3524be97f49f2b90a71e20820226
dev.alyzahran95 <dev.alyzahran95@gmail.com>
2024-08-25 14:57:20 +0300

    add view component update

---
+++ WeatheAppDemo/ViewComponents/CityWeatherViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatheAppDemo.Models;
+
+namespace WeatheAppDemo.ViewComponents
+{
+    public class CityWeatherViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather cityWeather, string color)
+        {
+            ViewBag.color = color;
+            return View("CityWeather", cityWeather);
+        }
+
+    }
+}
